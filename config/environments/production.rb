00f299594e16a967c9701cdf3220776f0066da09
thenicholashernandez <thenicholashernandez@gmail.com>
2024-03-29 12:13:51 +0800

    Edit production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://sample-app-oe1c.onrender.com'
+  host = 'https://sample-app-55d9.onrender.com'
   config.action_mailer.default_url_options = { host: host }
   config.action_mailer.smtp_settings = {
     address: 'smtp.sendgrid.net',
